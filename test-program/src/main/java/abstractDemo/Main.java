c82b46209c777c87c2f3f2d9cd87fa75e0eb1173
MatWro <91191153+MatWro@users.noreply.github.com>
2023-05-30 15:34:32 +0200

    abstract classes

---
+++ test-program/src/main/java/abstractDemo/Main.java
@@ -0,0 +1,4 @@
+package abstractDemo;
+
+public class Main {
+}
---
+++ test-program/src/main/java/abstractDemo/Son.java
@@ -0,0 +1,17 @@
+package abstractDemo;
+
+public class Son extends Parent{
+    public Son(int age) {
+        super(age);
+    }
+
+    @Override
+    void career() {
+
+    }
+
+    @Override
+    void partner() {
+
+    }
+}
